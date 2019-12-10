7a8e6641433e72f0204914d7593668aa051b72d6
Robin Siep <RobinSiep1@gmail.com>
2019-12-10 12:24:20 +0100

    rough implementation of sysinfo functions

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ sio = socketio.Client()
 @sio.event
 def connect():
     print('connection established')
+    identify()
+
+
+def identify():
+    sio.emit('identify', {
+        'name': 'Oak',
+        'ip': '0.0.0.0'
+    })
 
 
 @sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+def sys_info(_):
+    return {
+        'cpu': '100%'
+    }
 
 
 @sio.event
