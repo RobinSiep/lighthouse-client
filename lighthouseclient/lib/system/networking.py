40b5492ee9b4d9b0a8a0a463c0d4bf5ac52ae949
Robin Siep <RobinSiep1@gmail.com>
2020-05-19 16:07:03 +0200

    fast port scanner

--- lighthouseclient/lib/system/networking.py
+++ lighthouseclient/lib/system/networking.py
@@ -1,3 +1,4 @@
+import asyncio
 from socket import gethostname
 
 import requests
@@ -39,3 +40,54 @@ class Network:
     @staticmethod
     def get_external_ip_address():
         return requests.get(EXTERNAL_IP_SERVICE).text
+
+
+class PortScanner:
+    host = '127.0.0.1'
+    max_workers = 200
+
+    task_queue = asyncio.Queue(maxsize=max_workers)
+    out_queue = asyncio.Queue()
+    scan_completed = asyncio.Event()
+
+    def __init__(self, timeout=0.1):
+        self.timeout = timeout
+
+    async def scan_port_from_queue(self):
+        while True:
+            port = await self.task_queue.get()
+            conn = asyncio.open_connection(self.host, port)
+            try:
+                await asyncio.wait_for(conn, self.timeout)
+            except asyncio.TimeoutError:
+                pass
+            else:
+                self.out_queue.put_nowait(port)
+            finally:
+                self.task_queue.task_done()
+
+    async def task_manager(self, max_port):
+        for port in range(1, max_port):
+            await self.task_queue.put(port)
+        self.scan_completed.set()
+
+    async def scan(self, max_port=49152):
+        self.scan_completed.clear()
+        tasks = [asyncio.create_task(self.task_manager(max_port))]
+
+        for _ in range(self.max_workers):
+            tasks.append(asyncio.create_task(self.scan_port_from_queue()))
+
+        await self.scan_completed.wait()
+        await self.task_queue.join()
+
+        for task in tasks:
+            task.cancel()
+
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+        open_ports = []
+        while self.out_queue.qsize():
+            open_ports.append(self.out_queue.get_nowait())
+
+        return open_ports
--- lighthouseclient/network.py
+++ lighthouseclient/network.py
@@ -2,6 +2,7 @@ import re
 from subprocess import call
 
 from lighthouseclient import sio
+from lighthouseclient.lib.system.networking import PortScanner
 
 MAC_ADDRESS_PATTERN = '[0-9A-F]{2}([-:]?)[0-9A-F]{2}(\\1[0-9A-F]{2}){4}$'
 
@@ -11,3 +12,11 @@ def send_wake_on_LAN_packet(recipient_mac):
     if not re.match(MAC_ADDRESS_PATTERN, recipient_mac):
         raise ValueError("recipient_mac is not a valid MAC address.")
     call(('wakeonlan', recipient_mac))
+
+
+@sio.event
+async def emit_ports():
+    print('called')
+    test = await PortScanner().scan()
+    print('done')
+    return test
